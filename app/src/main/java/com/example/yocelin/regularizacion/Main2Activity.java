7e4b691efe56ed79000db13ebe26bab313b3e8fe
yocelin del carmen perez guzman <yocelin-princesa@hotmail.com>
2018-11-27 15:49:27 -0600

    cambio

--- app/src/main/java/com/example/yocelin/regularizacion/Main2Activity.java
+++ app/src/main/java/com/example/yocelin/regularizacion/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.yocelin.regularizacion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    private void cambio2 (View view){
+        Intent objI = new Intent(Main2Activity.this,Main3Activity.class);
+        startActivity(objI);
     }
 }
--- app/src/main/java/com/example/yocelin/regularizacion/MainActivity.java
+++ app/src/main/java/com/example/yocelin/regularizacion/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.yocelin.regularizacion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        }
+     private void cambio (View view){
+            Intent objI = new Intent(MainActivity.this,Main2Activity.class);
+            startActivity(objI);
+     }
 }
+
